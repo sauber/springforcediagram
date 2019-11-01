399cb35237dfca6fff6c0430be1ae3338d6592fe
Søren Døssing <netcom@sauber.net>
2019-11-01 17:55:16 +0100

    Render multiline text

--- spec/text.js
+++ spec/text.js
@@ -13,7 +13,15 @@ describe("Text", function () {
     var text = new Text("a");
     expect(text.area()).toBe(1*1);
 
-    text.value = "a b";
-    expect(text.area()).toBe(1*3);
+    text.value = "a bcd";
+    expect(text.area()).toBe(1*5);
+
+    text.taller();
+    expect(text.area()).toBe(2*3);
+    expect(text.desired_line_count).toBe(2);
+
+    text.taller();
+    expect(text.area()).toBe(2*3);
+    expect(text.desired_line_count).toBe(2);
   });
 });
--- src/text.js
+++ src/text.js
@@ -13,9 +13,56 @@ class Text {
     return this.value.split(/\s+/);
   }
 
-  // Join words into lines
+  // Join words into lines, trying to match desired number of lines
   lines () {
-    return [ this.words().join(' ') ];
+    var words = this.words();
+    if ( this.desired_line_count == 1 ) {
+      return [ words.join(' ') ];
+    }
+
+    var width;
+    var lines;
+    var index;
+    // console.log(this.min_width());
+    // console.log(this.max_width());
+    const min = this.min_width();
+    const max = this.max_width();
+    for ( width = min; width <= max; width++ ) {
+      // console.log(width);
+      lines = [''];
+      for ( index = 0; index < words.length; index++ ) {
+        var new_length;
+        var last_line = lines.length - 1; // index of last line
+
+        if ( lines[last_line].length > 0 ) {
+          new_length = lines[last_line].length + 1 + words[index].length;
+        } else {
+          new_length = words[index].length;
+        }
+
+        // if lines would become too long then add new line
+        if ( new_length > width ) {
+          lines.push('');
+          last_line++;
+        }
+
+        if ( lines[last_line].length > 0 ) {
+          lines[last_line] = [lines[last_line], words[index]].join(' ');
+        } else {
+          lines[last_line] = words[index];
+        }
+      }
+      if ( lines.length <= this.desired_line_count ) {
+        // console.log("The count of lines became " + lines.length);
+        // console.log(lines);
+        if ( this.desired_line_count > lines.length ) {
+          this.desired_line_count = lines.length;
+        }
+        // console.log(this.desired_line_count);
+        return lines;
+      }
+    }
+    // console.log("Reached end of loop");
   }
   
   area () {
@@ -23,6 +70,27 @@ class Text {
     //console.log(lines);
     return lines.length * Math.max(...lines);
   }
+
+  // Attempt to make more lines of output
+  taller() {
+    this.desired_line_count++;
+  }
+
+  // Size of longest word
+  min_width () {
+    //console.log(this.words());
+    var word_lengths = this.words().map(x => x.length);
+    //console.log(word_lengths);
+    var max = Math.max(...word_lengths);
+    //console.log(max);
+    return Math.max(...word_lengths);
+  }
+
+  // Length of all words joined
+  max_width () {
+    return this.words().join(' ').length;
+  }
+
 }
 
 module.exports = Text;
