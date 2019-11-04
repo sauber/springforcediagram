2f09f9f97596ff8f02a7489bcac1ca0640a8a443
Søren Døssing <netcom@sauber.net>
2019-11-04 11:47:24 +0100

    labels is a subnode

--- spec/node.js
+++ spec/node.js
@@ -1,16 +1,23 @@
-var Node = require("../src/node");
+const Node = require("../src/node");
 
 describe("Node", function () {
   it("should allow empty instantiation", function () {
-    var node = new Node;
+    const node = new Node;
   });
 
   it("should allow adding child nodes", function () {
-    var node = new Node;
+    const node = new Node;
     node.add("a");
     expect(node.children.length).toBe(1);
     node.add("b", "c");
     expect(node.children.length).toBe(3);
   });
 
+  it("may have a text sub node", function () {
+    const emptynode = new Node;
+    expect(emptynode.children.length).toBe(0);
+
+    const textnode = new Node("label");
+    expect(textnode.children.length).toBe(1);
+  });
 });
--- src/node.js
+++ src/node.js
@@ -3,11 +3,17 @@
 //   https://codereview.stackexchange.com/questions/226709/javascript-tree-class
 
 "use strict";
+const Text = require("./text");
 
 class Node {
-  constructor (value = 0, children = []) {
+  constructor (value = undefined, children = []) {
     this.value = value;
     this.children = children;
+    if ( value ) {
+      // Add a text shape as sub node
+      const label = new Text(value);
+      this.children.unshift(label);
+    }
   }
 
   add (...children) {
